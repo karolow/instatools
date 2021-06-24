86940b5ddbda406ac6590c77271720dc4b80d799
karolow <karol.piekarski@gmail.com>
2021-06-24 10:29:03 +0200

    remove analysis module

--- instatools/analysis/__init__.py
+++
@@ -1,4 +0,0 @@
-from .analysis import (
-    most_common_hashtags,
-    generate_lda_viz
-)
--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='instagramtools',
     version='0.0.1',
-    description='A collection of tools to facilitate acquisition and analysis of data from the Instagram API',
+    description='A collection of tools to facilitate acquisition and preprocessing of data from the Instagram API',
     packages=find_packages(include=['instatools', 'instatools.*'])
 )
